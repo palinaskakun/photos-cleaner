e2db28893cd54cdd69a4ca46780e75d5652e4246
Palina Skakun <palinaskakun@gmail.com>
2024-12-28 21:07:03 +0100

    Initial Commit

---
+++ Photos Cleaner/Photos_CleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photos_CleanerApp.swift
+//  Photos Cleaner
+//
+//  Created by Palina Skakun on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Photos_CleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Photos CleanerTests/Photos_CleanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Photos_CleanerTests.swift
+//  Photos CleanerTests
+//
+//  Created by Palina Skakun on 12/28/24.
+//
+
+import Testing
+@testable import Photos_Cleaner
+
+struct Photos_CleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
